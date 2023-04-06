4c0b70000c9ea0aa674ef9d6925ea189a737838f
sojos204 <67076560+sojos204@users.noreply.github.com>
2023-04-05 20:56:27 -0600

    add main page and navigation menu

--- frontend/frontend-customer/src/app/app-routing.module.ts
+++ frontend/frontend-customer/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './components/body/body.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { LocationPageComponent } from './components/location-page/location-page.component';
+import { RatesPageComponent } from './components/rates-page/rates-page.component';
+import { ReservationPageComponent } from './components/reservation-page/reservation-page.component';
+import { ContactsPageComponent } from './components/contacts-page/contacts-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BodyComponent,
+  children: [ {
+    path: 'home',
+    component: HomePageComponent,
+  },{
+    path: 'about',
+    component: AboutPageComponent,
+  }
+  ,{
+    path: 'location',
+    component: LocationPageComponent,
+  },{
+    path: 'rates',
+    component: RatesPageComponent,
+  },{
+    path: 'reservation',
+    component: ReservationPageComponent,
+  },{
+    path: 'contacts',
+    component: ContactsPageComponent,
+  }]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/frontend-customer/src/app/app.module.ts
+++ frontend/frontend-customer/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './components/body/body.component';
+import { NavbarComponent } from './components/body/navbar/navbar.component';
+import { MainMenuComponent } from './components/body/main-menu/main-menu.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { RatesPageComponent } from './components/rates-page/rates-page.component';
+import { LocationPageComponent } from './components/location-page/location-page.component';
+import { ContactsPageComponent } from './components/contacts-page/contacts-page.component';
+import { ReservationPageComponent } from './components/reservation-page/reservation-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    NavbarComponent,
+    MainMenuComponent,
+    AboutPageComponent,
+    HomePageComponent,
+    RatesPageComponent,
+    LocationPageComponent,
+    ContactsPageComponent,
+    ReservationPageComponent
   ],
   imports: [
     BrowserModule,
